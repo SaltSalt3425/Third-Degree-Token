79dbdef33385b528e077462e214984521aa124bc
skyge <18844062006@163.com>
2020-11-26 01:02:33 +0800

    :pushpin: Add <package.json>: Add dependencies.

---
+++ hardhat.config.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+require("@nomiclabs/hardhat-waffle");
+
+const privateKey = process.env.PRIVATE_KEY;
+const infuraKey = process.env.INFURA_KEY;
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks: {
+    kovan: {
+      url: `https://kovan.infura.io/v3/${infuraKey}`,
+      accounts: [`0x${privateKey}`],
+    },
+  },
+  solidity: {
+    version: "0.6.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 2000,
+      },
+    },
+  },
+};
+
