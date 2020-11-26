f9f88e1035776dc6813a7be4e17eec68a2f2affb
skyge <18844062006@163.com>
2020-11-27 01:02:36 +0800

    :building_construction: Update <TDTToken.sol>: Update the formula when burn.

--- contracts/TDTToken.sol
+++ contracts/TDTToken.sol
@@ -29,55 +29,24 @@ contract TDTToken is Initializable, OwnableUpgradeable, ERC20Upgradeable, Reentr
      * @dev Hook that is called before any transfer of tokens, at here, we would like to
      *      burn token and charge fee when `_transfer()`
      */
-    function _beforeTransfer(address from, uint256 amount, uint256 burnAmount, uint256 fakeSupply) internal {
-        uint256 currentSupply = fakeSupply != 0? fakeSupply : totalSupply();
-        uint256 totalBurnAmount = burnAmount != 0 ? burnAmount : 0;
-        uint256 remainingSpare;
-        uint256 fakeSupplyDecreased;
+    function _beforeTransfer(address from, uint256 amount) internal {
+        uint256 currentSupply = totalSupply();
+        uint256 burnAmount;
         require(amount <= currentSupply, "_beforeTransfer: Too much to transfer!");
 
         // If 7,000 < total supply <= 10,000, burn fee is 3%, tx fee is 7%
         if (currentSupply > thresholdOne) {
-            remainingSpare = currentSupply.sub(thresholdOne);
-            if (remainingSpare > amount) {
-                totalBurnAmount = amount.mul(3).div(100).add(totalBurnAmount);
-            } else {
-                // TODO: need to have a test!
-                fakeSupplyDecreased = remainingSpare.mul(3).div(100);
-                totalBurnAmount = fakeSupplyDecreased.add(totalBurnAmount);
-                fakeSupplyDecreased = totalSupply().sub(fakeSupplyDecreased).sub(remainingSpare);
-                _beforeTransfer(from, amount.sub(remainingSpare), totalBurnAmount, fakeSupplyDecreased);
-            }
+            burnAmount = amount.mul(3).div(100);
         } else if (currentSupply > thresholdTwo) {
             // If 4,000 < total supply <= 7,000, burn fee is 6%, tx fee is 14%
-            remainingSpare = currentSupply.sub(thresholdTwo);
-            if (remainingSpare > amount) {
-                totalBurnAmount = amount.mul(6).div(100).add(totalBurnAmount);
-            } else {
-                // TODO:
-                fakeSupplyDecreased = remainingSpare.mul(6).div(100);
-                totalBurnAmount = fakeSupplyDecreased.add(totalBurnAmount);
-                fakeSupplyDecreased = totalSupply().sub(fakeSupplyDecreased).sub(remainingSpare);
-                _beforeTransfer(from, amount.sub(remainingSpare), totalBurnAmount, fakeSupplyDecreased);
-            }
+            burnAmount = amount.mul(6).div(100);
         } else if (currentSupply > thresholdThree) {
             // If 300 < total supply <= 4,000, burn fee is 9%, tx fee is 21%
-            remainingSpare = currentSupply.sub(thresholdThree);
-            if (remainingSpare > amount) {
-                totalBurnAmount = amount.mul(9).div(100).add(totalBurnAmount);
-            } else {
-                // TODO:
-                fakeSupplyDecreased = remainingSpare.mul(6).div(100);
-                totalBurnAmount = fakeSupplyDecreased.add(totalBurnAmount);
-                fakeSupplyDecreased = totalSupply().sub(fakeSupplyDecreased).sub(remainingSpare);
-                _beforeTransfer(from, amount.sub(remainingSpare), totalBurnAmount, fakeSupplyDecreased);
-            }
+            burnAmount = amount.mul(9).div(100);
         } else {
             // if total supply <= 300, no burn fee, but tx fee is 3%
         }
-        // TODO:
-        require(totalBurnAmount != 0, "_beforeTransfer: Too small to transfer!");
-        _burn(from, totalBurnAmount);
+        _burn(from, burnAmount);
     }
 
     /**
@@ -107,7 +76,7 @@ contract TDTToken is Initializable, OwnableUpgradeable, ERC20Upgradeable, Reentr
             );
         }
 
-        _beforeTransfer(spender, amount, 0, 0);
+        _beforeTransfer(spender, amount);
 
         _transfer(spender, recipient, amount);
 
